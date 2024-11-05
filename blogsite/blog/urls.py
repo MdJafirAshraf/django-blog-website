e4f02a3f127de68e561efbc7746ab561e718881e
Jafir Ashraf <mdjafirashraf@gmail.com>
2024-11-05 09:32:46 +0530

    add urls in update & delete blog

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from .views import (
     blog_list_view,
     blog_create,
-    blog_post_details
+    blog_post_details,
+    blog_update,
+    blog_delete
     )
 
 urlpatterns = [
     # app views path
     path('', blog_list_view),
     path('<str:slug>/', blog_post_details),
+    path('<str:slug>/edit', blog_update),
+    path('<str:slug>/delete', blog_delete),
 ]
