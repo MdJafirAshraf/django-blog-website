a19c15ff81ad9cf6584e359a2d781fb7391aa76d
Jafir Ashraf <mdjafirashraf@gmail.com>
2024-11-06 11:57:42 +0530

    create fn for urls

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -5,3 +5,12 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(null=True)
     content = models.TextField(null=False,  blank=False)
+
+    def get_absolute_url(self):
+        return f"/blogpost/{self.slug}"
+    
+    def get_edit_url(self):
+        return f"/blogpost/{self.slug}/edit"
+    
+    def get_delete_url(self):
+        return f"/blogpost/{self.slug}/delete"
